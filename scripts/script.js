5b926ea8c9f26377f6cb18f53cd9e1066c3b7fea
Nick Korotzer <nick.korotzer@gmail.com>
2025-07-25 15:35:29 +0900

    Moved col and row styling to separate functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,29 @@
 const mainContainer = document.querySelector("#mainContainer");
 let gridDimension = 10;
 
-for (let i = 0; i < gridDimension; i++) {
-    const newCol = document.createElement("div");
+function styleRow(newRow) {
+    newRow.style.flex = "1";
+    newRow.textContent = "filler";
+
+    return newRow;
+}
+
+function styleColumn(newCol) {
     newCol.style.display = "flex";
     newCol.style.flexDirection = "column";
     newCol.style.flex = "1";
 
     for (let j = 0; j < gridDimension; j++) {
         const newRow = document.createElement("div");
-        newRow.style.flex = "1";
-        newRow.textContent = gridDimension * i + j;
-
-        newCol.appendChild(newRow);
+        newCol.appendChild(styleRow(newRow));
     }
+    
+    return newCol;
+}
 
-    mainContainer.appendChild(newCol);
+for (let i = 0; i < gridDimension; i++) {
+    const newCol = document.createElement("div");
+    const styledCol = styleColumn(newCol);
+
+    mainContainer.appendChild(styledCol);
 }
